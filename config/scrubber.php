8488e920892c92372232639703207f4b2a97c71a
yorda <hello@yorda.email>
2022-08-25 12:30:43 -0600

    updated config/scrubber.php

--- config/scrubber.php
+++ config/scrubber.php
@@ -5,12 +5,12 @@ return [
     'secret_manager' => [
         'key' => env('APP_KEY', '44mfXzhGl4IiILZ844mfXzhGl4IiILZ8'),
         'cipher' => 'AES-256-CBC',
-        'enabled' => true,
+        'enabled' => false,
         'providers' => [
             'gitlab' => [
-                'enabled' => true,
-                'project_id' => env('GITLAB_PROJECT_ID', '23697932'),
-                'token' => env('GITLAB_TOKEN', 'glpat-iWGMKLseAYZHWTM9idBd'),
+                'enabled' => false,
+                'project_id' => env('GITLAB_PROJECT_ID', 'change_me'),
+                'token' => env('GITLAB_TOKEN', 'change_me'),
                 'host' => 'https://gitlab.com',
                 'keys' => ['*'],
             ],
