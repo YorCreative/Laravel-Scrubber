fe552ab3d604c27a6e451e0ed9e09c9c111a1659
Yorda <yordadevs@gmail.com>
2022-11-05 17:25:55 -0600

    Tap Loader Strategy (#16)
    * Tap Loader Strategy
    
    - Added strategy to load channel taps.
    - Added 3 loaders: wildcard, specific and multiple channel tap loaders.
    - Added test coverage for tap loader strategy.
    - Added "tap_channels" to config with wildcard default.
    - Added log channel opt-in information to the readme.

---
+++ src/Strategies/TapLoader/Loaders/DefaultChannels.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace YorCreative\Scrubber\Strategies\TapLoader\Loaders;
+
+use Illuminate\Config\Repository;
+use YorCreative\Scrubber\Handlers\ScrubberTap;
+
+class DefaultChannels extends WildCardChannel
+{
+    /**
+     * @return bool
+     */
+    final public function canLoad(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param  Repository  $config
+     */
+    public function load(Repository &$config): void
+    {
+        $channels = $config->get('logging.channels');
+
+        foreach ($channels as $key => $channel) {
+            $config->set("logging.channels.$key.tap", [
+                ScrubberTap::class,
+            ]);
+        }
+    }
+}
---
+++ src/Strategies/TapLoader/Loaders/MultipleChannel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace YorCreative\Scrubber\Strategies\TapLoader\Loaders;
+
+use Illuminate\Config\Repository;
+use Illuminate\Support\Facades\Config;
+use YorCreative\Scrubber\Handlers\ScrubberTap;
+use YorCreative\Scrubber\Strategies\TapLoader\TapLoaderInterface;
+
+class MultipleChannel implements TapLoaderInterface
+{
+    /**
+     * @return bool
+     */
+    public function canLoad(): bool
+    {
+        $channels = Config::get('scrubber.tap_channels');
+        if (! $channels) {
+            return false;
+        }
+
+        return ! in_array('*', Config::get('scrubber.tap_channels'))
+            && count(Config::get('scrubber.tap_channels')) > 1;
+    }
+
+    /**
+     * @param  Repository  $config
+     */
+    public function load(Repository &$config): void
+    {
+        foreach (Config::get('scrubber.tap_channels') as $channel) {
+            $config->set("logging.channels.$channel.tap", [
+                ScrubberTap::class,
+            ]);
+        }
+    }
+}
