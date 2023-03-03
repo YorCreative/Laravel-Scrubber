73f137975af7f0ab1bbc533eb7abca1912798772
Yorda <yordadevs@gmail.com>
2023-03-03 12:58:54 -0700

    update for failing test :construction:

--- tests/Unit/Strategies/RegexLoaderStrategyTest.php
+++ tests/Unit/Strategies/RegexLoaderStrategyTest.php
@@ -2,11 +2,10 @@
 
 namespace YorCreative\Scrubber\Test\Unit\Strategies;
 
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Config;
+use YorCreative\Scrubber\Tests\TestCase;
 use YorCreative\Scrubber\Repositories\RegexCollection;
 use YorCreative\Scrubber\Strategies\RegexLoader\RegexLoaderStrategy;
-use YorCreative\Scrubber\Tests\TestCase;
 
 class RegexLoaderStrategyTest extends TestCase
 {
@@ -18,10 +17,7 @@ class RegexLoaderStrategyTest extends TestCase
      */
     public function it_can_load_default_core()
     {
-        $regexClasses = new Collection();
-        app(RegexLoaderStrategy::class)->load($regexClasses);
-
-        $this->assertCount(25, $regexClasses);
+        $this->assertCount(25, app(RegexLoaderStrategy::class)->load());
     }
 
     /**
@@ -33,9 +29,7 @@ class RegexLoaderStrategyTest extends TestCase
     public function it_can_load_specific_core()
     {
         Config::set('scrubber.regex_loader', [RegexCollection::$GOOGLE_API]);
-        $regexClasses = new Collection();
-        app(RegexLoaderStrategy::class)->load($regexClasses);
 
-        $this->assertCount(1, $regexClasses);
+        $this->assertCount(1, app(RegexLoaderStrategy::class)->load());
     }
 }
