9529e6cd09730c9cf42d17153a8c54841517f95f
Jorge Anzola <jorgeanzola@gmail.com>
2023-04-06 14:15:46 +0200

    Added email regex

---
+++ src/RegexCollection/EmailRegex.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace YorCreative\Scrubber\RegexCollection;
+
+use YorCreative\Scrubber\Interfaces\RegexCollectionInterface;
+
+class EmailRegex implements RegexCollectionInterface
+{
+    public function getPattern(): string
+    {
+        return '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}';
+    }
+
+    public function getTestableString(): string
+    {
+        return 'example@test.com';
+    }
+
+    public function isSecret(): bool
+    {
+        return false;
+    }
+}
