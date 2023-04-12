c3e8f76fd841cb0ddbd5acaf96f9330795983335
Jorge Anzola <jorgeanzola@gmail.com>
2023-04-12 16:38:28 +0200

    Added EmailAddress regex (#30)
    * Added email regex
    
    * Improved the regex to allow longer tld
    
    * Updated failing test case to account for new core regex class.
    
    ---------
    
    Co-authored-by: Yorda <yordadevs@gmail.com>

---
+++ src/RegexCollection/EmailAddress.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace YorCreative\Scrubber\RegexCollection;
+
+use YorCreative\Scrubber\Interfaces\RegexCollectionInterface;
+
+class EmailAddress implements RegexCollectionInterface
+{
+    public function getPattern(): string
+    {
+        return '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}';
+    }
+
+    public function getTestableString(): string
+    {
+        return 'johndoe321@example.associates';
+    }
+
+    public function isSecret(): bool
+    {
+        return false;
+    }
+}
