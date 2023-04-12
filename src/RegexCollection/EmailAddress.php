636a0cd9c02d324f0fbd99f664fce0c796a7620f
Jorge Anzola <jorgeanzola@gmail.com>
2023-04-12 15:17:35 +0200

    Improved the regex to allow longer tld

--- src/RegexCollection/EmailAddress.php
+++ src/RegexCollection/EmailAddress.php
@@ -8,12 +8,12 @@ class EmailAddress implements RegexCollectionInterface
 {
     public function getPattern(): string
     {
-        return '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}';
+        return '[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}';
     }
 
     public function getTestableString(): string
     {
-        return 'example@test.com';
+        return 'johndoe321@example.associates';
     }
 
     public function isSecret(): bool
