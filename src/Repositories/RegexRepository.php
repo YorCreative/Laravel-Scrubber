423168689207c17c5c4ff17a9e30ede8f502ca7b
Jeroen Derks <info@magentron.com>
2023-11-07 00:44:53 +0100

    Update RegexRepository.php to use the PCRE modifier 'S' (#44)
    Increases sanitization performance in regex repository.  
    More information: https://www.php.net/manual/en/reference.pcre.pattern.modifiers.php

--- src/Repositories/RegexRepository.php
+++ src/Repositories/RegexRepository.php
@@ -13,12 +13,12 @@ class RegexRepository
 
     public static function checkAndSanitize(string $regex, string $replace, string $content, int &$hits = 0): string
     {
-        return preg_replace("~$regex~i", $replace, $content, -1, $hits);
+        return preg_replace("~$regex~Si", $replace, $content, -1, $hits);
     }
 
     public static function check(string $regex, string $content): int
     {
-        return preg_match_all("~$regex~i", $content);
+        return preg_match_all("~$regex~Si", $content);
     }
 
     public function getRegexCollection(): Collection
